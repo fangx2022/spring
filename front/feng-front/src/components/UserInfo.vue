cfd21ff2d54fb47354d2ead38faf88ea805f8f49
fangx <fxabcde@yeah.net>
2024-02-28 23:52:49 +0800

    1.前端element-ui vue2代码提交，使用axios调用后端接口

---
+++ front/feng-front/src/components/UserInfo.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="userId" label="用户编号" width="180"> </el-table-column>
+    <el-table-column prop="username" label="用户姓名" width="180"> </el-table-column>
+    <el-table-column prop="password" label="密码" width="180"> </el-table-column>
+    <el-table-column prop="age" label="年龄"> </el-table-column>
+    <el-table-column prop="createDate" label="创建日期"> </el-table-column>
+  </el-table>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+export default {
+  // 生命周期 组件被创建时加载
+  created:function(){
+    this.$axios.get("/mp/query").then(response=>{
+      console.log(response.data)
+      this.tableData = response.data
+    })
+  },
+  // 生命周期 组件被挂载完毕
+  mounted:function(){
+    console.log("组件挂载完毕")
+  },
+  data() {
+    return {
+      tableData: [
+      ],
+    };
+  },
+};
+</script>
---
+++ front/feng-front/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';//element-ui内部组件用到的样式
+import 'font-awesome/css/font-awesome.min.css'//图标库
+
+import axios from 'axios'
+axios.defaults.baseURL = "http://localhost:8888"
+Vue.prototype.$axios=axios
+
+Vue.config.productionTip = false
+Vue.use(ElementUI);//全局注册
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
